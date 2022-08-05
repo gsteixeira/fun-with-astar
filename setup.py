0d927f97d1e9c47d57661b62698e0d10a637934b
Gustavo S. Teixeira <gsteixei@gmail.com>
2022-08-05 00:20:33 -0300

    update package name

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='astar',
+    name='python-astar',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='GNU License',
     description='A simplistic A* path search tool',
     long_description=README,
+    long_description_content_type='text/markdown',
     url='',
     author='Gustavo Selbach Teixeira',
     author_email='gsteixei@gmail.com',
